87ca0ad48dc23eb781e71f687571263340f5ab9e
Meredith Matthews <meredith.matthews@t-mark.com>
2016-12-23 15:14:57 -0500

    meatpile

--- meatpile.js
+++ meatpile.js
@@ -1,4 +1,11 @@
-(function deathpill(fizz,buzz,start,end){
-	console.log((start%fizz === 0 ? "death" : "")+(start%buzz === 0 ? "pill" : "")|| start);
-	start < end ? deathpill(fizz,buzz,start+1,end) : null; 
+(function meatpile(fizz,buzz,start,end){
+	var result= '';
+	switch(true){
+		case start%(fizz*buzz) === 0 : result += 'meatpile'; break;
+		case start%fizz === 0 : result += 'meat'; break;
+		case start%buzz === 0 : result += 'pile'; break;
+		default: result = start; break;
+	}
+	console.log(result);
+	start < end ? meatpile(fizz,buzz,start+1,end) : null; 
 }(3,5,1,100));
